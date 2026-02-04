1d20c9416e2d81312a245db34a9ebaa984c6d932
Marco Lorenzo Arigoni <marco.arigoni03@gmail.com>
2026-02-04 21:56:53 +0100

    Clean

--- open-node.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-docker run -it --rm -p 5173:5173 -v "$(pwd):/src" -w "/src/webui" node:20 /bin/bash
--- webui/src/services/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const instance = axios.create({
-	baseURL: "http://localhost:3000", // Punta al tuo backend Go
-	timeout: 1000 * 5
-});
-
-export default instance;
