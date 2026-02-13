d176c2e87c23560ad7501fe97a26c5c74a4372e1
marcoarigoni03-coder <marco.arigoni03@gmail.com>
2026-02-13 15:35:46 +0100

    Remove comments from api-context-wrapper.go

--- service/api/api-context-wrapper.go
+++ service/api/api-context-wrapper.go
@@ -8,11 +8,10 @@ import (
 	"net/http"
 )
 
-// httpRouterHandler is the signature for functions that accepts a reqcontext.RequestContext in addition to those
-// required by the httprouter package.
+
 type httpRouterHandler func(http.ResponseWriter, *http.Request, httprouter.Params, reqcontext.RequestContext)
 
-// wrap parses the request and adds a reqcontext.RequestContext instance related to the request.
+
 func (rt *_router) wrap(fn httpRouterHandler) func(http.ResponseWriter, *http.Request, httprouter.Params) {
 	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 		reqUUID, err := uuid.NewV4()
@@ -25,13 +24,13 @@ func (rt *_router) wrap(fn httpRouterHandler) func(http.ResponseWriter, *http.Re
 			ReqUUID: reqUUID,
 		}
 
-		// Create a request-specific logger
+	
 		ctx.Logger = rt.baseLogger.WithFields(logrus.Fields{
 			"reqid":     ctx.ReqUUID.String(),
 			"remote-ip": r.RemoteAddr,
 		})
 
-		// Call the next handler in chain (usually, the handler function for the path)
+		
 		fn(w, r, ps, ctx)
 	}
 }
